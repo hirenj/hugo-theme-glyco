6d01c3b47fde965ddb5b54e1b02ae1f98613fa1e
Hiren Joshi <hirenj@gmail.com>
2021-07-09 15:52:45 +0200

    Move across to direct DOI resolution

--- src/components/citation.vue
+++ src/components/citation.vue
@@ -1,8 +1,8 @@
 <template>
-<div>
-  <template v-if="loading"><div>Loading{{doi}}</div></template>
+<div style="display: contents;">
+  <template v-if="loading"><a :href="'https://doi.org/'+doi">{{doi}}</a></template>
   <template v-else>
-    <div v-if="citation !== ''"><a target="_blank" :href="'https://doi.org/'+citation.DOI">{{citation.author[0].family || citation.author[0].name }} <em>et al</em> {{citation['short-container-title'][0]}} {{citation.issued['date-parts'][0][0]}}</a></div>
+    <div style="display: contents;" v-if="citation !== ''"><a target="_blank" :href="'https://doi.org/'+citation.DOI">{{citation.author[0].family || citation.author[0].name }} <em>et al.</em> {{citation['container-title-short']}} {{citation.issued['date-parts'][0][0]}}</a></div>
     <div v-else>Reference not available</div>
   </template>
 </div>
@@ -16,10 +16,12 @@ var get_citation = (doi) => {
   if (CITATION_CACHE[doi]) {
     return CITATION_CACHE[doi];
   }
-  const crossref_uri = `https://api.crossref.org/works/${doi}`;
-  CITATION_CACHE[doi] = fetch(crossref_uri).then( resp => resp.json() ).then( data => data.message );
 
-  //Promise.resolve({ author: [ {name:"Foo"} ], "short-container-title": ["Bar"], issued: { "date-parts" : [["1234"]]} }); 
+  const crossref_uri = `https://api.crossref.org/works/${doi}`;
+
+  const doi_uri = `https://doi.org/${doi}`;
+  CITATION_CACHE[doi] = fetch(doi_uri, { headers: { Accept: 'application/vnd.citationstyles.csl+json'}}).then( resp => resp.json() );
+
   return CITATION_CACHE[doi];
 };
 
