e8a45821299b29978a6b18b985c6224d4d159f50
Hiren Joshi <hirenj@gmail.com>
2024-05-31 14:48:16 +0200

    Make alignment calculator more robust

--- script/calculate_alignment.sh
+++ script/calculate_alignment.sh
@@ -3,14 +3,33 @@
 
 function get_sequence {
   wanted_id="$1"
-  curl "https://rest.uniprot.org/uniprotkb/search?query=$wanted_id&fields=accession,sequence" | jq --raw-output '.results | .[] | [.primaryAccession, .sequence.value ] | @tsv'
+  temp_seq=$(mktemp)
+  curl --silent --output $temp_seq "https://rest.uniprot.org/uniprotkb/search?query=$wanted_id&fields=accession,sequence"
+  is_inactive=$(jq --raw-output '.results | .[] | .entryType == "Inactive"' < $temp_seq)
+  if [[ ! "$is_inactive" == "true" ]]; then
+    jq --raw-output '.results | .[] | [.primaryAccession, .sequence.value ] | @tsv' < $temp_seq
+  else
+    >&2 echo "Error $wanted_id"
+    return 1
+  fi
 }
 
 sequences_file=$(mktemp)
 
+seq_errors=0
+
 for seqid in "$@"; do
 	get_sequence "$seqid"
+  if [[ $? -gt 0 ]]; then
+    seq_errors=1
+  fi
 done > $sequences_file
+
+
+if [[ $seq_errors -gt 0 ]]; then
+  >&2 echo "Skipping alignment due to errors"
+  exit 1
+fi
 
 sequences=$(cut -f2 "$sequences_file" | tr '\n' ',' | sed -e 's/,$//')
 
@@ -22,11 +41,12 @@ if [[ -f ".env.prod" ]]; then
 	. .env.prod
 fi
 
-auth_token=$(curl -H "x-api-key: $CLIENT_ID" "$ROOT_API/api/login" | jq --raw-output '.id_token')
+auth_token=$(curl --silent -H "x-api-key: $CLIENT_ID" "$ROOT_API/api/login" | jq --raw-output '.id_token')
 
 raw_alignment=$(mktemp)
 
 curl -H "x-api-key: $CLIENT_ID" \
+  --silent \
   -H "Authorization: Bearer $auth_token" \
   -d "sequences=$sequences" \
   -H "Content-Type: application/x-www-form-urlencoded" \
